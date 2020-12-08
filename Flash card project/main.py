cf7a6f176f583a4b5f0f896a4c687d03c8dd187b
Yeison Casado <Yeisontech2020@gmail.com>
2020-12-07 23:50:22 -0500

    Add files via upload

---
+++ Flash card project/main.py
@@ -0,0 +1,101 @@
+from tkinter import *
+import pandas
+from random import *
+to_learn = {}
+current_card = {}
+#
+# #----------Read Funtion -----------------#
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+    
+except: 
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn= original_data.to_dict(orient="record")
+    
+else: 
+        
+    to_learn = data.to_dict(orient="records")
+
+
+
+def next_card():
+    global current_card, flip_timer
+    
+    window.after_cancel(flip_timer)
+    current_card = choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index = False)
+
+
+
+
+#-------------------------------------------------------------------
+
+BACKGROUND_COLOR = "#B1DDC6"
+window = Tk()
+window.title("Flash Card App")
+window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
+
+
+#-------- Getting the next card-------------------------------#
+
+#  Setting up first image front image check all the steps
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+# NOte this highlightthickness=0 takes out the border of my image
+
+
+
+#----------------------Button_unkown-----------------------
+crosse_image= PhotoImage(file="images/wrong.png")
+unknown = Button(image=crosse_image,  highlightthickness=0, command = next_card)
+unknown.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image,  highlightthickness=0, command = is_known)
+known_button.grid(column=1, row = 1)
+
+next_card()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
